4f2c22ef03712a52ba441b522d858e72f6a98c4e
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-01 21:02:43 -0400

    Fixed: 'prisma' is possibly 'undefined'

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,6 +3,7 @@ import { PrismaClient } from '@prisma/client';
 declare global {
   var prisma: PrismaClient | undefined;
 }
+let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
