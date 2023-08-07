1d0014272cb321d79a5c2fc9dc5c1747024dcecd
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-07 19:04:37 -0400

    Revert trips badges

--- app/trip_management/table.tsx
+++ app/trip_management/table.tsx
@@ -20,12 +20,12 @@ import {
 const TRIP_UNASSIGNED = 0, TRIP_ASSIGNED = 1, TRIP_COMPLETED = 2;
 
 const colors: { [key: number]: string } = {
-  // [TRIP_UNASSIGNED]: "badge-outline",
-  // [TRIP_ASSIGNED]: "badge-secondary",
-  // [TRIP_COMPLETED]: "badge-primary",
-  [TRIP_UNASSIGNED]: "neutral",
-  [TRIP_ASSIGNED]: "yellow",
-  [TRIP_COMPLETED]: "green",
+  [TRIP_UNASSIGNED]: "badge-outline",
+  [TRIP_ASSIGNED]: "badge-secondary",
+  [TRIP_COMPLETED]: "badge-primary",
+  // [TRIP_UNASSIGNED]: "neutral",
+  // [TRIP_ASSIGNED]: "yellow",
+  // [TRIP_COMPLETED]: "green",
 };
 
 type Trip = {
@@ -76,13 +76,13 @@ export default function TripsTable({ trips }: { trips: any }) {
               </TableCell>
               <TableCell>
                 {/* <span className="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"> */}
-                {/* <div className={`badge ${colors[trip.completion]}`}> */}
-                <Badge color={`${colors[trip.completion]}`}>
+                <div className={`badge ${colors[trip.completion]}`}>
+                {/* <Badge color={`${colors[trip.completion]}`}> */}
                   {trip.completion === TRIP_UNASSIGNED && 'Unassigned'}
                   {trip.completion === TRIP_ASSIGNED && 'Assigned'}
                   {trip.completion === TRIP_COMPLETED && 'Completed'}
-                </Badge>
-                {/* </div> */}
+                {/* </Badge> */}
+                </div>
                 {/* </span> */}
               </TableCell>
               <TableCell>
