d671bbc5a208766c16c31c9327c730b4ada2f30f
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-10 15:04:01 -0400

    Add trip dropdown menu

--- app/trip_management/columns.tsx
+++ app/trip_management/columns.tsx
@@ -1,6 +1,18 @@
 "use client"
 
 import { ColumnDef } from "@tanstack/react-table"
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "../../@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../../@/components/ui/dropdown-menu"
+
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -66,10 +78,33 @@ export const columns: ColumnDef<Trips>[] = [
     },
   },
   {
+    // This column is hidden. The output combined with the 'Cases Picked' column
     accessorKey: "total_cases",
-    // header: () => <div className="text-right">Cases Picked/Total Cases</div>,
-    // cell: ({ row }) => {
-    //   return <div className="text-right font-medium">{row.getValue("total_cases")}</div>
-    // },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => { 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => {alert('Details')}}
+            >
+              Trip Details
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Assign</DropdownMenuItem>
+            <DropdownMenuItem>Unassign</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ]
