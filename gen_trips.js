7bdaf7e3dc0dc490b584e12d42531e0b669164ef
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-15 11:48:15 -0400

    Generate trips based on completion type

--- gen_trips.js
+++ gen_trips.js
@@ -1,5 +1,6 @@
 const { faker } = require('@faker-js/faker');
 const { Pool } = require('pg');
+const { start } = require('repl');
 require('dotenv').config();
 
 const { DATABASE_URL } = process.env;
@@ -14,9 +15,9 @@ const pool = new Pool({
 const MAX_EMPLOYEES = 100, MAX_CASES = 400, MAX_DOORS = 30;
 const MIN_ROUTES = 30, MAX_ROUTES = 50, MAX_STOPS = 4;
 const MIN_TRIPS_PER_DAY = 400, TRIPS_PER_DAY_RANDOM_RANGE = 200, TRIP_LENGTH_RANDOM_RANGE = 1000 * 60 * 60 * 0.5;
-const CASE_WEIGHT = 50, CASE_TIME = 13 * 1000;  // 13 seconds per case
+const CASE_WEIGHT = 30, CASE_TIME = 13 * 1000;  // 13 seconds per case
 
-const START_DATE = new Date('2023-08-01'), END_DATE = new Date();
+const START_DATE = new Date('2023-08-15'), END_DATE = new Date();
 
 // TODO: Only assign trips to employees who orderfillers or lift drivers
 // TODO: Make start and end times for trips assigned to the same employee on the same day not overlap
@@ -35,11 +36,11 @@ const START_DATE = new Date('2023-08-01'), END_DATE = new Date();
     - Set completion to random number from 0 to 2
       - If completion is 1,
         - Set cases_picked to random number from 0 to total_cases
-        - Set employee_id to random number from 1 to totalEmployees
-        - Set start_time to random time
+        - Set employee_id to random number from 1 to MAX_EMPLOYEES
+        - Set start_time to random time for the day
       - If completion is 2,
         - Set cases_picked to total_cases
-        - Set employee_id to random number from 1 to totalEmployees
+        - Set employee_id to random number from 1 to MAX_EMPLOYEES
         - Set start_time to random time
         - Set end_time to start_time plus standard_time - (TRIP_LENGTH_RANDOM_RANGE / 2) + random number from 0 to TRIP_LENGTH_RANDOM_RANGE minutes
 */
@@ -56,18 +57,30 @@ function genWeight(totalCases){
 } 
 
 function generateRandomTrip(curDate) {
-  let completion = 0;
   let date = curDate;
   let route = faker.number.int({min: 1, max: MAX_ROUTES});
   let stop = faker.number.int({min: 1, max: MAX_STOPS});
   let total_cases = faker.number.int({min: 1, max: MAX_CASES});
   let weight = genWeight(total_cases);
-  let cases_picked = 0;
-  let employee_id = null;
   let door = faker.number.int({min: 1, max: MAX_DOORS});
   let standard_time = CASE_TIME * total_cases;
-  let start_time = Date.now();
-  let end_time = start_time + (standard_time / 2) + TRIP_LENGTH_RANDOM_RANGE * Math.random();
+  
+  let completion = faker.number.int({min: 0, max: 2});
+  let cases_picked = 0;
+  let employee_id = null;
+  let start_time, end_time;
+
+  if (completion !== 0) {
+    employee_id = faker.number.int({min: 1, max: MAX_EMPLOYEES});
+    start_time = faker.date.between({ from: date, to: new Date(date.getTime() + 1000 * 60 * 60 * 24) });
+    end_time = start_time.getTime() + (standard_time / 2) + TRIP_LENGTH_RANDOM_RANGE * Math.random();
+  }
+
+  if (completion === 1) {
+    cases_picked = faker.number.int({min: 0, max: total_cases});
+  } else if (completion === 2) {
+    cases_picked = total_cases;
+  }
 
   // Used for determining order filler performance
   let actual_time = end_time - start_time;
@@ -78,7 +91,7 @@ function generateRandomTrip(curDate) {
   // const formattedDate = parsedDate.toISOString().replace("T", " ").replace(/\.\d+/, "");
   // console.log(formattedDate);
 
-  return [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, parseDate(new Date(start_time)), parseDate(new Date(end_time)), convertMillisecondsToTime(standard_time), convertMillisecondsToTime(actual_time)];
+  return [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time ? parseDate(new Date(start_time)) : null, end_time ? parseDate(new Date(end_time)) : null, convertMillisecondsToTime(standard_time)];
   // return {completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, standard_time: convertMillisecondsToTime(standard_time), start_time: new Date(start_time), end_time: new Date(end_time), performance};
 }
 
@@ -93,11 +106,11 @@ function convertMillisecondsToTime(ms) {
 async function insertTrips() {
   let client = await pool.connect();
   try {
-    const query = 'INSERT INTO trips (completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time, actual_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *';
-    // const values = [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time, actual_time];
+    const query = 'INSERT INTO trips (completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *';
+    // const values = [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time];
     for (let curDate = START_DATE; curDate <= END_DATE; curDate.setDate(curDate.getDate() + 1)) {
-      // let numTrips = MIN_TRIPS_PER_DAY + Math.floor(Math.random() * TRIPS_PER_DAY_RANDOM_RANGE);
-      let numTrips = 1;
+      let numTrips = MIN_TRIPS_PER_DAY + Math.floor(Math.random() * TRIPS_PER_DAY_RANDOM_RANGE);
+      // let numTrips = 1;
   
       for (let curTrip = 1; curTrip <= numTrips; curTrip++) {
   
@@ -114,5 +127,5 @@ async function insertTrips() {
 
 insertTrips();
 
-// console.log(generateRandomTrip());
-// generateRandomTrip()
+// console.log(generateRandomTrip(new Date()));
+// generateRandomTrip(new Date())
