fb8a3f94d9a93ae912e43ecc64ad012ba70153d0
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-15 16:29:13 -0400

    Fix implicit type and remove 'rows selected'

--- components/data-table-pagination.tsx
+++ components/data-table-pagination.tsx
@@ -25,15 +25,15 @@ export function DataTablePagination<TData>({
   return (
     <div className="flex items-center justify-between px-2">
       <div className="flex-1 text-sm text-muted-foreground">
-        {table.getFilteredSelectedRowModel().rows.length} of{" "}
-        {table.getFilteredRowModel().rows.length} row(s) selected.
+        {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
+        {table.getFilteredRowModel().rows.length} row(s) selected. */}
       </div>
       <div className="flex items-center space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
-            onValueChange={(value) => {
+            onValueChange={(value: number) => {
               table.setPageSize(Number(value))
             }}
           >
