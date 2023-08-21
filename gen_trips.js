68ccd7e74cf3d39cf5e9a927f4d5864a25de245c
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-21 02:17:20 -0400

    Updated gen_trips, fixed NaN performance bug

--- gen_trips.js
+++ gen_trips.js
@@ -13,13 +13,12 @@ const pool = new Pool({
 
 const MAX_EMPLOYEES = 100, MAX_CASES = 400, MAX_DOORS = 30;
 const MIN_ROUTES = 30, MAX_ROUTES = 50, MAX_STOPS = 4;
-const MIN_TRIPS_PER_DAY = 400, TRIPS_PER_DAY_RANDOM_RANGE = 200, TRIP_LENGTH_RANDOM_RANGE = 1000 * 60 * 60 * 0.5;
+const MIN_TRIPS_PER_DAY = 400, TRIPS_PER_DAY_RANDOM_RANGE = 200, TRIP_LENGTH_RANDOM_RANGE = 1000 * 60 * 60 * 0.5; // 30 minute range
 const CASE_WEIGHT = 30, CASE_TIME = 13 * 1000;  // 13 seconds per case
 
 const MS_PER_HOUR = 1000 * 60 * 60;
 
-const START_DATE = new Date('01-01-2023'), END_DATE = new Date('08-16-2023');
-// const START_DATE = new Date(), END_DATE = new Date();
+const TODAYS_DATE = new Date(new Date().setHours(0, 0, 0, 0));
 
 // TODO: Only assign trips to employees who are orderfillers or lift drivers
 // TODO: Make start and end times for trips assigned to the same employee on the same day not overlap
@@ -67,33 +66,54 @@ function generateRandomTrip(curDate) {
   let door = faker.number.int({min: 1, max: MAX_DOORS});
   let standard_time = CASE_TIME * total_cases;
   
-  let completion = faker.number.int({min: 0, max: 2});
+  let completion;
+  if (curDate.toString() !== TODAYS_DATE.toString()) {
+    completion = 2;
+  } else {
+    completion = faker.number.int({min: 0, max: 2});
+  }
+
   let cases_picked = 0;
   let employee_id = null;
   let start_time, end_time;
+  let performance = null;
 
   if (completion !== 0) {
     employee_id = faker.number.int({min: 1, max: MAX_EMPLOYEES});
     start_time = faker.date.between({ from: date, to: new Date(date.getTime() + MS_PER_HOUR * 24) });
-    end_time = start_time.getTime() + (standard_time / 2) + TRIP_LENGTH_RANDOM_RANGE * Math.random();
   }
 
   if (completion === 1) {
     cases_picked = faker.number.int({min: 0, max: total_cases});
   } else if (completion === 2) {
     cases_picked = total_cases;
-  }
+    end_time = start_time.getTime() + (standard_time / 2) + faker.number.int(standard_time);  // Range is 0.5 standard time to 1.5 standard time
 
-  // Used for determining order filler performance
-  let actual_time = end_time - start_time;
-  let performance = ((standard_time / actual_time) * 100).toFixed(2);
+    // Determine order filler performance
+    let actual_time = end_time - start_time;
+    performance = ((standard_time / actual_time) * 100).toFixed(2);
+  }
 
   // console.log(total_cases, convertMillisecondsToTime(standard_time), convertMillisecondsToTime(actual_time));
   // let parsedDate = new Date(start_time);
   // const formattedDate = parsedDate.toISOString().replace("T", " ").replace(/\.\d+/, "");
   // console.log(formattedDate);
 
-  return [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time ? parseDate(new Date(start_time)) : null, end_time ? parseDate(new Date(end_time)) : null, convertMillisecondsToTime(standard_time)];
+  return [
+    completion,
+    weight, 
+    route, 
+    stop, 
+    total_cases, 
+    cases_picked, 
+    date, 
+    employee_id, 
+    door,
+    start_time ? parseDate(new Date(start_time)) : null,
+    end_time ? parseDate(new Date(end_time)) : null,
+    convertMillisecondsToTime(standard_time),
+    performance
+  ];
   // return {completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, standard_time: convertMillisecondsToTime(standard_time), start_time: new Date(start_time), end_time: new Date(end_time), performance};
 }
 
@@ -105,38 +125,25 @@ function convertMillisecondsToTime(ms) {
   return formattedTime;
 }
 
-async function insertTrips(singleDate = false) {
+async function insertTrips(startDate = TODAYS_DATE, endDate = TODAYS_DATE) {
   let client = await pool.connect();
+  
   try {
-    const query = 'INSERT INTO trips (completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *';
+    const query = 'INSERT INTO trips (completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time, performance) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *';
     // const values = [completion, weight, route, stop, total_cases, cases_picked, date, employee_id, door, start_time, end_time, standard_time];
-
-    console.log('Inserting trips...');
     
-    if (!singleDate) {    
-      for (let curDate = START_DATE; curDate <= END_DATE; curDate.setDate(curDate.getDate() + 1)) {
-        let numTrips = MIN_TRIPS_PER_DAY + Math.floor(Math.random() * TRIPS_PER_DAY_RANDOM_RANGE);
-        // let numTrips = 1;
-        
-        console.log(curDate);
-    
-        for (let curTrip = 1; curTrip <= numTrips; curTrip++) {
-    
-          const values = generateRandomTrip(curDate);
-          
-          const res = await client.query(query, values);
-          // console.log(res.rows[0]);
-        }
-      }
-    } else {
-      const curDate = new Date(new Date().setHours(0, 0, 0, 0));
+    for (let curDate = startDate; curDate <= endDate; curDate.setDate(curDate.getDate() + 1)) {
       let numTrips = MIN_TRIPS_PER_DAY + Math.floor(Math.random() * TRIPS_PER_DAY_RANDOM_RANGE);
+      // let numTrips = 1;
       
       console.log(curDate);
   
       for (let curTrip = 1; curTrip <= numTrips; curTrip++) {
+  
         const values = generateRandomTrip(curDate);
+        
         const res = await client.query(query, values);
+        // console.log(res.rows[0]);
       }
     }
   } finally {
@@ -144,10 +151,13 @@ async function insertTrips(singleDate = false) {
   }
 }
 
-if (process.argv[2] === 'single') {
-  insertTrips(true);
-} else {
+if ((new Date(process.argv[2])).toString() !== 'Invalid Date' && (new Date(process.argv[3])).toString() !== 'Invalid Date') {
+  console.log(`Inserting trips from ${process.argv[2]} to ${process.argv[3]}...`);
+  insertTrips(new Date(process.argv[2]), new Date(process.argv[3]));
+} else if (process.argv.length === 2) {
+  console.log(`Inserting trips for ${TODAYS_DATE}...`);
   insertTrips();
+} else if ((new Date(process.argv[2])).toString() === 'Invalid Date' || (new Date(process.argv[3])).toString() === 'Invalid Date') {
+  console.log('Invalid date(s)');
+  console.log('Please enter two valid dates (ex: gen_trips 01-01-2023 01-31-2023) or no dates to (ex: gen_trips)');
 }
-
-// console.log(generateRandomTrip(new Date()));
