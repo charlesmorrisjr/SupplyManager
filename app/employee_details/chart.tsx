2a29fbdd071ee1dbd866bab0da12d81950cc4d2b
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-21 13:45:30 -0400

    Fix chart bugs, only show when > 1 trip

--- app/employee_details/chart.tsx
+++ app/employee_details/chart.tsx
@@ -23,10 +23,12 @@ function populateData(trips: any) {
   data.length = 0;
 
   trips.forEach((trip: any) => {
-    data.push({
-      baseline: 100.00,
-      percentage: trip.performance,
-    });
+    if (trip.completion === 2) {
+      data.push({
+        baseline: 100.00,
+        percentage: trip.performance,
+      });
+    }
   });
 }
 
@@ -56,89 +58,96 @@ export function Chart({ trips, performance }: { trips: any, performance: any }) 
           Employee Performance for today.
         </CardDescription>
       </CardHeader>
-      <CardContent className="pb-4">
-        <div className="h-[200px]">
-          <ResponsiveContainer>
-            <LineChart
-              data={data}
-              margin={{
-                top: 100,
-                right: 10,
-                left: 10,
-                bottom: 0,
-              }}
-            >
-              <Tooltip
-                content={({ active, payload }) => {
-                  if (active && payload && payload !== undefined && payload.length) {
-                    return (
-                      <div className="rounded-lg border bg-background p-2 shadow-sm">
-                        <div className="grid grid-cols-2 gap-2">
-                          <div className="flex flex-col">
-                            <span className="text-[0.70rem] uppercase text-muted-foreground">
-                              Baseline
-                            </span>
-                            <span className="font-bold text-muted-foreground">
-                              {payload[0].value}%
-                            </span>
-                          </div>
-                          <div className="flex flex-col">
-                            <span className="text-[0.70rem] uppercase text-muted-foreground">
-                              Trip
-                            </span>
-                            <span className="font-bold">
-                              {payload[1].value}%
-                            </span>
+        <CardContent className="pb-4">
+          <div className="h-[200px]">
+
+          { data.length > 1 ?
+            <ResponsiveContainer>
+              <LineChart
+                data={data}
+                margin={{
+                  top: 100,
+                  right: 10,
+                  left: 10,
+                  bottom: 0,
+                }}
+              >
+                <Tooltip
+                  content={({ active, payload }) => {
+                    if (active && payload && payload !== undefined && payload.length) {
+                      return (
+                        <div className="rounded-lg border bg-background p-2 shadow-sm">
+                          <div className="grid grid-cols-2 gap-2">
+                            <div className="flex flex-col">
+                              <span className="text-[0.70rem] uppercase text-muted-foreground">
+                                Baseline
+                              </span>
+                              <span className="font-bold text-muted-foreground">
+                                {payload[0].value}%
+                              </span>
+                            </div>
+                            <div className="flex flex-col">
+                              <span className="text-[0.70rem] uppercase text-muted-foreground">
+                                Trip
+                              </span>
+                              <span className="font-bold">
+                                {payload[1].value}%
+                              </span>
+                            </div>
                           </div>
                         </div>
-                      </div>
-                    )
-                  }
+                      )
+                    }
 
-                  return null
-                }}
-              />
-              <Line
-                type="monotone"
-                strokeWidth={2}
-                dataKey="baseline"
-                activeDot={{
-                  r: 6,
-                  style: { fill: "black", opacity: 0.25 },
-                }}
-                style={
-                  {
-                    stroke: "black",
-                    opacity: 0.25,
-                    color: "var(--theme-primary)",
-                    // "--theme-primary": `hsl(${
-                    //   theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
-                    // })`,
-                  } as React.CSSProperties
-                }
-              />
-              <Line
-                type="monotone"
-                dataKey="percentage"
-                strokeWidth={2}
-                activeDot={{
-                  r: 8,
-                  style: { fill: "black" },
-                }}
-                style={
-                  {
-                    stroke: "black",
-                    color: "var(--theme-primary)",
-                    // "--theme-primary": `hsl(${
-                    //   theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
-                    // })`,
-                  } as React.CSSProperties
-                }
-              />
-            </LineChart>
-          </ResponsiveContainer>
-        </div>
-      </CardContent>
+                    return null
+                  }}
+                />
+                <Line
+                  type="monotone"
+                  strokeWidth={2}
+                  dataKey="baseline"
+                  activeDot={{
+                    r: 6,
+                    style: { fill: "black", opacity: 0.25 },
+                  }}
+                  style={
+                    {
+                      stroke: "black",
+                      opacity: 0.25,
+                      color: "var(--theme-primary)",
+                      // "--theme-primary": `hsl(${
+                      //   theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
+                      // })`,
+                    } as React.CSSProperties
+                  }
+                />
+                <Line
+                  type="monotone"
+                  dataKey="percentage"
+                  strokeWidth={2}
+                  activeDot={{
+                    r: 8,
+                    style: { fill: "black" },
+                  }}
+                  style={
+                    {
+                      stroke: "black",
+                      color: "var(--theme-primary)",
+                      // "--theme-primary": `hsl(${
+                      //   theme?.cssVars[mode === "dark" ? "dark" : "light"].primary
+                      // })`,
+                    } as React.CSSProperties
+                  }
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          :
+            <div className="flex flex-col items-center justify-center h-full">
+              <span className="text-muted-foreground">Not enough data to display chart.</span>
+            </div>
+          }
+         </div>
+        </CardContent>
     </Card>
   )
 }
