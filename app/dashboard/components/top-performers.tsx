8bef9f44aaaa665ec695c178be718c06a5075c92
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-22 19:04:29 -0400

    Update Top Performers loading animation

--- app/dashboard/components/top-performers.tsx
+++ app/dashboard/components/top-performers.tsx
@@ -8,6 +8,8 @@ import {
   AvatarImage,
 } from "@/components/ui/avatar"
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 import useSWR from 'swr';
 
 export function TopPerformers() {
@@ -28,11 +30,13 @@ export function TopPerformers() {
   // * Uncomment the following line to have the table refresh every second
   // const { data, error } = useSWR([ '/api/trips', dateValue ], fetcher, { refreshInterval: 1000 });
   if (error) return <div>An error occurred.</div>
+
   if (data) {
-    // Get number of completed trips
     data.forEach((employee: any) => {
+      // Get number of completed trips
       let completedTrips = employee.trips.filter((trip: any) => trip.completion === 2).length;
       
+      // Calculate average performance by adding up all performance values and dividing by number of completed trips
       if (completedTrips > 0) {
         employee.performance = (employee.trips.reduce((total: number, trip: any) => {
           if (trip.completion === 2) {
@@ -46,9 +50,8 @@ export function TopPerformers() {
       }
     });
 
-    data.sort((a: any, b: any) => {
-      return Number(b.performance) - Number(a.performance);
-    });
+    // Sort employees by performance
+    data.sort((a: any, b: any) => Number(b.performance) - Number(a.performance));
   }
 
   return (
@@ -67,7 +70,11 @@ export function TopPerformers() {
           </div>
         ))
       ) : (
-        <div>Loading...</div>
+        [...Array(5)].map((_, i) => (
+          <div key={i} className="flex items-center">
+            <Skeleton className="w-[100%] h-9 rounded-full" />
+          </div>
+        ))
       )}
 
     </div>
