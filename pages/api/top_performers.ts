117b6c50412e0a7aea1c746d53b31b03a4023863
Charles Morris Jr <cmorrisjr777@gmail.com>
2023-08-23 21:42:28 -0400

    Fix Top Performers date bug

--- pages/api/top_performers.ts
+++ pages/api/top_performers.ts
@@ -2,13 +2,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-export default async function getEmployees(req: NextApiRequest, res: NextApiResponse) {
-  // Parse date to format YYYY-MM-DD and set time to 00:00:00
-  // This ensures that the passed in date is the same as the date in the database
-  const parseDate = (date: string) => new Date(new Date(date).toISOString().replace("T", " ").replace(/\.\d+/, "").split(" ")[0]);
-
+export default async function getTopPerformers(req: NextApiRequest, res: NextApiResponse) {
   // Get current date with time in format YYYY-MM-DDT00:00:00Z
-  const searchDate = new Date(new Date().toISOString().split('T')[0]);
+  const searchDate = new Date(new Date().setHours(0,0,0,0))
   
   const allEmployees = await prisma.employees.findMany({
     include: {
@@ -19,7 +15,6 @@ export default async function getEmployees(req: NextApiRequest, res: NextApiResp
         },
         where: {
           date: searchDate
-          // date: new Date('08-16-2023')
         }
       }
     },
